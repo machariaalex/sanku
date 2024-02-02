d6b78d92b00780552d281c781ea5a6fa93177fcc
Alex Macharia <87443384+machariaalex@users.noreply.github.com>
2024-02-02 09:08:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ from sklearn.metrics import confusion_matrix
 
 # Assuming you have the balanced_train and test_data available
 # If not, make sure to load your datasets appropriately
-balanced_train = pd.read_csv('balnsa.csv',index_col='SN')
+balanced_train = pd.read_csv('balan.csv')
 
-test_data = pd.read_csv('tsst.csv',index_col='SN')
+test_data = pd.read_csv('test.csv')
 
 #balanced_train.index = balanced_train.index.astype(int)
 #test_data.index = test_data.index.astype(int)
